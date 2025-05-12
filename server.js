3b5ba8fd9a2a74cd304b12428dda8ffc24b51d64
bmm333 <81256935+bmm333@users.noreply.github.com>
2025-05-12 21:28:08 +0000

    a

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-const express= require('express');
-const app= express();
-const port=3000;
-const db=require('./db');
-const path=require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./db');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  next();
-});
-
 const tasksRouter = require('./tasks');
-app.use('/api/tasks',tasksRouter);
-app.use(express.static(path.join(__dirname, 'frontend')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
-});
-app.listen(port, () => {
-    const tasks = [
-        { id: 1, description: 'Compito 1', important: false },
-        { id: 2, description: 'Compito 2', important: true }
-      ];
-    console.log(`server in ascolto su http://localhost:${port}`);
-});
+app.use('/api/tasks', tasksRouter);
 
+app.use(express.static('frontend'));
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
